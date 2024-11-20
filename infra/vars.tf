131296ec01f9b674dd73f42f48f49aca6a0778a2
Luhan Lacerda <luhanlacerda@gmail.com>
2024-11-20 17:28:09 -0300

    update vars.tf

--- infra/vars.tf
+++ infra/vars.tf
@@ -7,9 +7,11 @@ variable "aws_region" {
 variable "subnet_ids" {
   description = "Lista de IDs das subnets"
   type        = list(string)
+  default     = ["subnet-0e30ba8519b6badbc", "subnet-01a5ce07590a99018"]
 }
 
 variable "vpc_id" {
   description = "VPC ID"
   type        = string
+  default     = "vpc-060f9c0343485cb7b"
 }
