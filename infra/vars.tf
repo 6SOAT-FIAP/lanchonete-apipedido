d98434ff0bfc5436e09b34ca2da15c036d80c10d
Luhan Lacerda <luhanlacerda@gmail.com>
2024-11-20 17:05:55 -0300

    infra: ajuste para rodar ecs no aws academy

--- infra/vars.tf
+++ infra/vars.tf
@@ -1,68 +1,15 @@
-variable "projectName" {
-  default = "lanchonete-apipedido"
-}
-
-variable "clusterName" {
-  default = "lanchonete-apipedido-cluster"
-}
-
-variable "vpcCIDR" {
-  default = "172.31.0.0/20"
-}
-
-variable "rdsUser" {
-  description = "Inserir usuario do banco em secrets"
-  type        = string
-  sensitive   = true
-}
-
-variable "rdsPass" {
-  description = "Inserir senha do banco em secrets"
-  type        = string
-  sensitive   = true
-}
-
-variable "project_name_dynamo" {
-  description = "Nome do projeto. Por exemplo, 'bluesburguer'."
-  default     = "payment"
-  type        = string
-}
-
-variable "tags" {
-  type = map(string)
-  default = {
-    App      = "lanchonete-apipedido",
-    Ambiente = "Desenvolvimento"
-  }
-}
-
 variable "aws_region" {
-  default = "sa-east-1"
-}
-
-variable "aws_access_key" {
-  default = "sua_access_key"
-}
-
-variable "aws_secret_key" {
-  default = "sua_secret_key"
-}
-
-variable "domain_name" {
-  description = "Inserir aws secret key"
+  description = "Região da AWS"
   type        = string
-  default     = "lanchonete-apipedido.terraform.com"
+  default     = "us-east-1"
 }
 
-variable "github_repo_url" {
-  default = "https://github.com/6SOAT-FIAP/lanchonete-apipedido"
+variable "subnet_ids" {
+  description = "Lista de IDs das subnets"
+  type        = list(string)
 }
 
-
-locals {
-  tags = {
-    created_by = "terraform"
-  }
-
-  aws_ecr_url = "${data.aws_caller_identity.current.account_id}.dkr.ecr.${var.aws_region}.amazonaws.com"
+variable "vpc_id" {
+  description = "VPC ID"
+  type        = string
 }
